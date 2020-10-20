ceb7d33791d2b696dfba39e7c3a45b3b64a1c68c
sunithaaa <suninavi45@gmail.com>
2020-10-20 12:57:49 +0530

    complete airbnb web app with hardcoded data

--- src/Search.js
+++ src/Search.js
@@ -5,8 +5,10 @@ import "react-date-range/dist/styles.css" //main css file
 import "react-date-range/dist/theme/default.css"//theme css file
 import {Button} from "@material-ui/core"
 import PeopleIcon from "@material-ui/icons/People"
+import {useHistory} from "react-router-dom"
 //DATE PICKET COMPONENT
 function Search() {
+    const history=useHistory();
     const [startDate,setStartDate]=useState(new Date());
     const [endDate,setEndDate]=useState(new Date());
     const selectionRange={
@@ -26,7 +28,7 @@ function Search() {
             />
             <h2>Number of Guests <PeopleIcon/></h2>
             <input min={0} defaultValue={2} type="number"  />
-            <Button>Search Airbnb</Button>
+            <Button onClick={()=>history.push("./search")}>Search Airbnb</Button>
         </div>
     )
 }
